68d0471fdba5b5bb90b73239a7ad5f9b072f2fad
Alice W. <wenyaxuan0925@outlook.com>
2024-04-24 22:30:04 -0400

    test

--- src/tests/fs_tests.rs
+++ src/tests/fs_tests.rs
@@ -26,14 +26,15 @@ pub mod fs_tests {
         // ut_lind_fs_file_link_unlink();
         /* Pass */
         // ut_lind_fs_file_lseek_past_end();
+        // ut_lind_fs_fstat_complex();
+        // ut_lind_fs_getuid();
         /* --- */
-        ut_lind_fs_fstat_complex();
-        ut_lind_fs_getuid();
         // ut_lind_fs_load_fs();
-        // ut_lind_fs_mknod();
-        // ut_lind_fs_multiple_open();
-        // ut_lind_fs_rename();
-        // ut_lind_fs_rmdir();
+        /* Pass */
+        ut_lind_fs_mknod();
+        ut_lind_fs_multiple_open();
+        ut_lind_fs_rename();
+        ut_lind_fs_rmdir();
         // ut_lind_fs_stat_file_complex();
         // ut_lind_fs_stat_file_mode();
         // ut_lind_fs_statfs();
