7e39bf5e7435aa11fbe98071da35bdb02a3955fb
Alice W. <wenyaxuan0925@outlook.com>
2024-04-24 22:09:15 -0400

    fix ut_lind_fs_file_lseek_past_end

--- src/interface/file.rs
+++ src/interface/file.rs
@@ -230,7 +230,7 @@ impl EmulatedFile {
                         ptr = ptr.add(bytes_to_copy);
                     }
 
-                    if remain_len == 0 {
+                    if remain_len == 0 || length - remain_len == self.filesize {
                         break;
                     }
                 }
