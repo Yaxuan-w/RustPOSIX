50acdb284e8045f2d7b55a56a1dddc9f0bd7c196
Alice W. <wenyaxuan0925@outlook.com>
2024-04-26 11:34:23 -0400

    write bug

--- src/tests/fs_tests.rs
+++ src/tests/fs_tests.rs
@@ -67,7 +67,7 @@ pub mod fs_tests {
         // panic!("Something wrong{:?}", FILEOBJECTTABLE);
         assert_eq!(cage2.write_syscall(fd2, str2cbuf("hello there!"), 12), 12);
 
-        panic!("Something wrong{:?}", FILEOBJECTTABLE);
+        // panic!("Something wrong{:?}", FILEOBJECTTABLE);
         
         assert_eq!(cage2.lseek_syscall(fd2, 0, SEEK_SET), 0);
         let mut read_buf2_0 = sizecbuf(14);
