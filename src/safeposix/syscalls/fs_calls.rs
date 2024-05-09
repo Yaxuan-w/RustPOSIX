23905a056819e9aadaa133f2afc89380e0d0e6cb
Alice W. <wenyaxuan0925@outlook.com>
2024-05-09 18:00:04 -0400

    change mmap: add mmap

--- src/safeposix/syscalls/fs_calls.rs
+++ src/safeposix/syscalls/fs_calls.rs
@@ -1652,8 +1652,8 @@ impl Cage {
                             let _ret = unsafe { libc::mprotect(addr_para, len, PROT_READ | PROT_WRITE) };
 
                             // let mapaddr = unsafe{libc::mmap(addr_para, len, prot, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED, -1, off)};
-                            // let mapaddr = interface::libc_mmap(addr, len, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, -1, off);
-                            let map_addr = addr_para as *mut u8;
+                            let mapaddr = interface::libc_mmap(addr, len, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, -1, off);
+                            let map_addr = mapaddr as *mut u8;
                             let _ = fobj.readat(map_addr, len, off as usize);
                             let retaddr = ((addr_para as i64) & 0xffffffff) as i32;
                             return retaddr;
